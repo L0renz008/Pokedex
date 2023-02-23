ca29c71637eadeaa8b5146a8cfa29b4951c6a962
Lorenzo <lrenouvin98@gmail.com>
2023-02-23 16:49:23 +0100

    Card's UI completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import chevronRight from "../assets/chevron_right.svg";
 import chevronLeft from "../assets/chevron_left.svg";
 import pokeball from "../assets/pokeball.svg";
+import weight from "../assets/weight.svg";
+import height from "../assets/height.svg";
+import arrow_left from "../assets/arrow_left.svg";
 
 export default function Card() {
   return (
@@ -10,9 +13,9 @@ export default function Card() {
         <img src={pokeball} alt="pokeball" />
       </div>
       <div className="title">
-        <p>Retour</p>
+        <img src={arrow_left} alt="Retour" className="arrow left" />
         <h1>Bulbasaur</h1>
-        <p>#001</p>
+        <span>#001</span>
       </div>
       <div className="image">
         <img src={chevronLeft} alt="chevron-left" className="chevron left" />
@@ -32,9 +35,27 @@ export default function Card() {
           <h2>About</h2>
         </div>
         <div className="attributes">
-          <p>weigth</p>
-          <p>heigth</p>
-          <p>moves</p>
+          <div className="weight">
+            <div className="weight-info">
+              <img src={weight} alt="weight" className="img_weight" />
+              <span>6,9 kg</span>
+            </div>
+            <span>Weight</span>
+          </div>
+          <div className="height">
+            <div className="height-info">
+              <img src={height} alt="height" className="img_height" />
+              <span>0,7 m</span>
+            </div>
+            <span>Height</span>
+          </div>
+          <div className="abilities">
+            <div className="abilities-info">
+              <span>Chlorophyll</span>
+              <span>Overgrow</span>
+            </div>
+            <span>Abilities</span>
+          </div>
         </div>
         <div className="description">
           <p>
@@ -44,6 +65,50 @@ export default function Card() {
         </div>
         <div className="base-stats">
           <h2>Base Stats</h2>
+          <div className="base-stats-content">
+            <div className="label">
+              <p>HP</p>
+              <p>ATK</p>
+              <p>DEF</p>
+              <p>SpATK</p>
+              <p>SpDEF</p>
+              <p>SPE</p>
+            </div>
+            <div className="value">
+              <p>45</p>
+              <p>49</p>
+              <p>49</p>
+              <p>65</p>
+              <p>65</p>
+              <p>45</p>
+            </div>
+            <div className="chart">
+              <div
+                className="stat hp"
+                style={{ width: `${(45 * 100) / 200}%` }}
+              ></div>
+              <div
+                className="stat attack"
+                style={{ width: `${(49 * 100) / 200}%` }}
+              ></div>
+              <div
+                className="stat defense"
+                style={{ width: `${(49 * 100) / 200}%` }}
+              ></div>
+              <div
+                className="stat spe-attack"
+                style={{ width: `${(65 * 100) / 200}%` }}
+              ></div>
+              <div
+                className="stat spe-defense"
+                style={{ width: `${(65 * 100) / 200}%` }}
+              ></div>
+              <div
+                className="stat speed"
+                style={{ width: `${(45 * 100) / 200}%` }}
+              ></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
