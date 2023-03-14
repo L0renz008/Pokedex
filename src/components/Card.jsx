7c48fe5cdfe8901b7fd9b54ad1705763cad13711
Lorenzo <lrenouvin98@gmail.com>
2023-03-14 21:40:56 +0100

    get data from API

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import chevronRight from "../assets/chevron_right.svg";
 import chevronLeft from "../assets/chevron_left.svg";
 import pokeball from "../assets/pokeball.svg";
@@ -7,29 +7,80 @@ import height from "../assets/height.svg";
 import arrow_left from "../assets/arrow_left.svg";
 
 export default function Card() {
+  const [torti, setTorti] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getPokemonData() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/1000`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+
+    const poke = {
+      id: data.id,
+      name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+      height: data.height,
+      weight: data.weight,
+      abilities: data.abilities.map((ability) => ability.ability.name),
+      artwork: {
+        official: data.sprites.other["official-artwork"].front_default,
+        home: data.sprites.other["home"].front_default,
+      },
+      stats: {
+        hp: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        defense: data.stats[2].base_stat,
+        spattack: data.stats[3].base_stat,
+        spdefense: data.stats[4].base_stat,
+        speed: data.stats[5].base_stat,
+      },
+      types: data.types.map(
+        (type) =>
+          type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)
+      ),
+    };
+    console.log("poke = ", poke);
+    setTorti(poke);
+    setLoading(false);
+  }
+  useEffect(() => {
+    getPokemonData();
+  }, []);
+
   return (
     <div className="card-container">
       <div className="pokeball">
         <img src={pokeball} alt="pokeball" />
       </div>
       <div className="title">
-        <img src={arrow_left} alt="Retour" className="arrow left" />
-        <h1>Bulbasaur</h1>
-        <span>#001</span>
+        <img
+          src={arrow_left}
+          alt="Retour"
+          className="arrow left"
+          onClick={getPokemonData}
+        />
+        <h1>{torti?.name}</h1>
+        <span>#{torti?.id}</span>
       </div>
       <div className="image">
         <img src={chevronLeft} alt="chevron-left" className="chevron left" />
         <img
-          src="../img/bulbasaur.png"
-          alt="bulbasaur.png"
+          // src="../img/bulbasaur.png"
+          src={torti?.artwork.official}
+          alt={`${torti?.name}.png`}
           className="pokemon-img"
         />
         <img src={chevronRight} alt="chevron-right" className="chevron right" />
       </div>
       <div className="card-info">
         <div className="type-container">
-          <h2 className="type grass">Grass</h2>
-          <h2 className="type poison">Poison</h2>
+          {torti
+            ? torti.types.map((type) => (
+                <h2 className={`type ${type.toLowerCase()}`}>{type}</h2>
+              ))
+            : null}
         </div>
         <div className="about">
           <h2>About</h2>
