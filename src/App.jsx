2493f39d5b83d6cd7f35ad118e2bb9e20e7d9a1a
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-04-17 17:10:24 +0200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+
+import { Routes, Route, useSearchParams, BrowserRouter } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 
 import Card from "./components/Card";
@@ -6,7 +8,11 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <Card />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Card />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
