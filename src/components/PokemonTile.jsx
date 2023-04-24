7602db396ef13d2371f7a0a73c075add3349611f
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-04-24 17:02:16 +0200

    new structure with tiles for each pokemon

---
+++ src/components/PokemonTile.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+// import InfiniteScroll from "react-infinite-scroller";
+import Loading from "./Loading";
+
+export default function PokemonTile({ poke }) {
+  const [artwork, setArtwork] = useState();
+  const [id, setId] = useState();
+  const [isLoaded, setisLoaded] = useState(false);
+  const handleImgLoad = () => {
+    setisLoaded(true);
+  };
+  async function getArtwork() {
+    const res = await fetch(`${poke.url}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    setArtwork(data.sprites.other["official-artwork"].front_default);
+    setId(data.id);
+  }
+
+  useEffect(() => {
+    getArtwork();
+  }, []);
+
+  return (
+    <a href={`${poke.url.split("https://pokeapi.co/api/v2/pokemon").pop()}`}>
+      <span className="pokemon-id">#{id?.toString().padStart(4, "0")}</span>
+      <img style={{ display: "none" }} src={artwork} onLoad={handleImgLoad}></img>
+      {isLoaded ? <img style={{ height: "50px" }} src={artwork}></img> : <Loading />}
+
+      <li>{poke.name}</li>
+    </a>
+  );
+}
