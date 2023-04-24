cd6e9edcb15ea0429b40fa0acee441e28a598079
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-04-24 17:01:32 +0200

    refont of the structure

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,53 +1,73 @@
 import React, { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroller";
 import Loading from "./Loading";
+import PokemonTile from "./PokemonTile";
 
 export default function Pokedex() {
   const [listOfPoke, setListOfPoke] = useState([]);
+  const [pokedexSize, setPokedexSize] = useState();
   const [loading, setLoading] = useState(true);
-  async function getListOfPokemons(page = 1) {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${page * 1}`, {
+
+  async function getListOfPokemons(page) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${page * 3}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     });
     const data = await res.json();
     const list = data.results;
-    // console.log(list);
+
+    setPokedexSize(data.count);
     setListOfPoke(list);
     setLoading(false);
   }
-  // console.log(listOfPoke[0].url.split("https://pokeapi.co/api/v2/pokemon").pop());
+  // async function getArtwork(url) {
+  //   const res = await fetch(`${url}`, {
+  //     method: "GET",
+  //     headers: { "Content-Type": "application/json" },
+  //   });
+  //   const data = await res.json();
+  //   // console.log(data);
+  //   const artwork = data.sprites.other["official-artwork"].front_default;
+  //   console.log(artwork);
+  //   return artwork;
+  // }
+
   useEffect(() => {
     getListOfPokemons(1);
   }, []);
-  const items = listOfPoke.map((poke) => {
-    return <li>{poke.name}</li>;
-  });
-  // console.log(items);
+
   if (loading) return <Loading />;
   return (
     <>
-      <div>Pokédex</div>
-      {/* <div style={{ height: "700px;overflow:auto;" }}> */}
-        <InfiniteScroll
-          pageStart={1}
-          loadMore={getListOfPokemons}
-          hasMore={true}
-          loader={
-            <div className="loader" key={0}>
-              Loading ...
-            </div>
-          }
-        >
-          {items}
-        </InfiniteScroll>
-      {/* </div> */}
+      <div className="title">Pokédex</div>
 
-      {listOfPoke.map((poke) => (
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={getListOfPokemons}
+        hasMore={listOfPoke.length !== pokedexSize}
+        loader={<Loading />}
+        className="pokedex-body"
+      >
+        {listOfPoke.map((poke, index) => {
+          return (
+            <PokemonTile key={index} poke={poke} />
+            // <a
+            //   key={poke.name}
+            //   href={`${poke.url.split("https://pokeapi.co/api/v2/pokemon").pop()}`}
+            // >
+            //   <span className="pokemon-id">#9999</span>
+            //   <img src={getArtwork(poke.url)}></img>
+            //   <li>{poke.name}</li>
+            // </a>
+          );
+        })}
+      </InfiniteScroll>
+
+      {/* {listOfPoke.map((poke) => (
         <a key={poke.name} href={`${poke.url.split("https://pokeapi.co/api/v2/pokemon").pop()}`}>
           <li>{poke.name}</li>
         </a>
-      ))}
+      ))} */}
     </>
   );
 }
