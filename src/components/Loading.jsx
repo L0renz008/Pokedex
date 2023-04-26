ab723d1e217fabb808e37ac751a6c4f36117c05d
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-04-26 15:07:14 +0200

    refactor code

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import pokeball_color from "../assets/pokeball_color.svg";
 
+/**
+ * Component that displays the loading pokeball
+ */
 export default function Loading() {
   return (
     <div>
-      <img className="loading_pokeball" src={pokeball_color} alt="pokeball_color" />
+      <img
+        className="loading_pokeball"
+        src={pokeball_color}
+        alt="pokeball_color"
+      />
     </div>
   );
 }
