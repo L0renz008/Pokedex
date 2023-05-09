219bbf7d4e3f592d791abffef39ecdc549074cf9
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-05-09 16:06:06 +0200

    full switch to TypeScript done :done:

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Card from "./components/Card";
 import Pokedex from "./components/Pokedex";
-import NoMatch from "./components/NoMatch";
+import NoMatch from "./components/NotFound";
+import PokemonNotFound from "./components/PokemonNotFound";
 
 /**
  * Component that renders the app entirely with routing
- *
  */
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Routes>
           <Route path={`/`} element={<Pokedex />} />
           <Route path={`/pokemon/:id`} element={<Card />} />
+          <Route path={`/pokemon/not-found`} element={<PokemonNotFound />} />
           <Route path={`*`} element={<NoMatch />} />
         </Routes>
       </BrowserRouter>
