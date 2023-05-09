64a34f3ff176cc5d1bd07debc5f044906d1f8cf7
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-05-09 16:08:09 +0200

    special 404 page for incorrect id

---
+++ src/components/PokemonNotFound.tsx
@@ -0,0 +1,12 @@
+/**
+ * Component that returns the 404 page
+ * @returns
+ */
+export default function PokemonNotFound() {
+  return (
+    <>
+      <div>Error 404 - Pokemon you are searching for does not exist </div>
+      <a href="/">Go to Home</a>
+    </>
+  );
+}
