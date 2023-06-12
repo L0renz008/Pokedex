5bd9080d68b819e5558a630b3fa2541545ebed46
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-06-12 14:33:31 +0200

    switch interface to Type

--- src/components/types/PokemonTypes.ts
+++ src/components/types/PokemonTypes.ts
@@ -1,43 +1,49 @@
-export interface IPokemon {
+export type TPokemon = {
   id: number;
   name: string;
   height: number;
   weight: number;
   abilities: Array<string>;
-  artwork: { official: string; home: string; shiny: string };
-  stats: {
-    hp: number;
-    attack: number;
-    defense: number;
-    spattack: number;
-    spdefense: number;
-    speed: number;
-  };
+  artwork: TArtwork;
+  stats: TStats;
   types: Array<string>;
-}
-export interface IPokeData {
+  url?: string;
+};
+type TArtwork = {
+  official: string;
+  home: string;
+  shiny: string;
+};
+type TStats = {
+  hp: number;
+  attack: number;
+  defense: number;
+  spattack: number;
+  spdefense: number;
+  speed: number;
+};
+export type TPokemonAPI = {
   id: number;
   name: string;
   height: number;
   weight: number;
-  abilities: Array<IAbility>;
+  abilities: Array<TAbility>;
   sprites: {
     other: {
       home: { front_default: string; front_shiny: string };
       "official-artwork": { front_default: string; front_shiny: string };
     };
   };
-  stats: Array<IStat>;
-  types: Array<IType>;
-}
-export interface IAbility {
+  stats: Array<TStatsAPI>;
+  types: Array<TType>;
+};
+
+type TAbility = {
   ability: { name: string };
-}
-export interface IStat {
-  base_stat: number;
-}
-export interface IType {
+};
+type TType = {
   type: {
     name: string;
   };
-}
+};
+type TStatsAPI = { base_stat: number };
