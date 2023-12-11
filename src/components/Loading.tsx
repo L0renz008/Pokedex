4f983c455a0dd9d4321ff358001759584bab2988
Lorenzo Renouvin <lrenouvin@aubay.com>
2023-12-11 15:57:34 +0100

    minor bug fix

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,12 +5,12 @@ import pokeball_color from "../assets/pokeball_color.svg";
  */
 export default function Loading() {
   return (
-    <div>
+    <figure>
       <img
         className="loading_pokeball"
         src={pokeball_color}
         alt="pokeball_color"
       />
-    </div>
+    </figure>
   );
 }
